75c128363ea47d5a0d07a8604cc8c9a43b31a232
John <JFSwifty@hotmail.co.uk>
2019-07-07 18:18:25 +0100

    Added logic to the combinations counting, working with test data

--- ProblemsCode/CombinationsCounting.go
+++ ProblemsCode/CombinationsCounting.go
@@ -1,5 +1,27 @@
 package ProblemsCode
 
-func CombinationsCounting() {
+import (
+	"CodeAbbey/FunctionsPackage"
+	"fmt"
+)
 
+func CombinationsCounting() {
+	dataIn := FunctionsPackage.TextFileIn("\\InputFiles\\inputCombinationsCounting")
+	_, dataSlices := FunctionsPackage.StringToXRows(dataIn)
+
+	for _, i := range dataSlices {
+		n, k := FunctionsPackage.StringToInt(i[0]), FunctionsPackage.StringToInt(i[1])
+		fmt.Print(calcCombinations(n, k), " ")
+	}
+}
+
+func calcFactorial(n int) (result int) {
+	if n > 0 {
+		result = n * calcFactorial(n-1)
+		return result
+	}
+	return 1
+}
+func calcCombinations(n int, k int) int {
+	return calcFactorial(n) / (calcFactorial(k) * calcFactorial(n-k))
 }
