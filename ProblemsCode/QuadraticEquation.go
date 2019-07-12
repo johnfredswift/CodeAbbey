6b9469ef338b6e50ef2bad58de2f6c1d1e91f1e1
John <JFSwifty@hotmail.co.uk>
2019-07-12 16:12:32 +0100

    Fixed logic for Quadratic Equations, now works with both real and complex results

--- ProblemsCode/QuadraticEquation.go
+++ ProblemsCode/QuadraticEquation.go
@@ -13,42 +13,37 @@ func QuadraticEquation() {
 	for _, i := range dataSlice {
 		fmt.Println(i[0], i[1], i[2])
 		a, b, c := FunctionsPackage.StringToInt(i[0]), FunctionsPackage.StringToInt(i[1]), FunctionsPackage.StringToInt(i[2])
-		x1, x2, isI := findXForQuadratic(a, b, c)
-		if !isI {
-			answer := FunctionsPackage.FloatToString(x1) + " " + FunctionsPackage.FloatToString(x2)
-			answerSlice = append(answerSlice, answer)
-		} else {
-			answer := "0"
-			if x1 >= 0 {
-				answer += "+" + FunctionsPackage.FloatToString(x1) + "i 0"
-			} else {
-				answer += FunctionsPackage.FloatToString(x1) + "i 0"
-			}
-			if x2 >= 0 {
-				answer += "+" + FunctionsPackage.FloatToString(x2) + "i"
-			} else {
-				answer += FunctionsPackage.FloatToString(x2) + "i"
-			}
-			answerSlice = append(answerSlice, answer)
-		}
+		x1, x2 := findXForQuadratic(a, b, c)
+
+		answerSlice = append(answerSlice, x1+" "+x2)
+
 	}
 	for _, i := range answerSlice {
 		fmt.Print(i + "; ")
 	}
-	fmt.Println()
-	fmt.Println(len(answerSlice))
 
 }
 
-func findXForQuadratic(a int, b int, c int) (x1 float64, x2 float64, isI bool) {
+func findXForQuadratic(a int, b int, c int) (x1 string, x2 string) {
 	if math.Pow(float64(b), 2)-4*float64(a)*float64(c) < 0 {
-		isI = true
-		x1 = (float64(-b) + math.Sqrt(-(math.Pow(float64(b), 2) - 4*float64(a)*float64(c)))) / (float64(2 * a))
-		x2 = (float64(-b) - math.Sqrt(-(math.Pow(float64(b), 2) - 4*float64(a)*float64(c)))) / (float64(2 * a))
+		x1PartOne := FunctionsPackage.FloatToString(float64(-b) / (float64(2 * a)))
+		x2PartOne := FunctionsPackage.FloatToString(float64(-b) / (float64(2 * a)))
+		x1PartTwo := FunctionsPackage.FloatToString(
+			math.Sqrt(-(math.Pow(float64(b), 2)-4*float64(a)*float64(c)))/(float64(2*a))) + "i"
+		x2PartTwo := FunctionsPackage.FloatToString(
+			-(math.Sqrt(-(math.Pow(float64(b), 2) - 4*float64(a)*float64(c))) / (float64(2 * a)))) + "i"
+		if string(x1PartTwo[:1]) != "-" {
+			x1PartTwo = "+" + x1PartTwo
+		}
+		if string(x2PartTwo[:1]) != "-" {
+			x2PartTwo = "+" + x2PartTwo
+		}
+		x1 = x1PartOne + x1PartTwo
+		x2 = x2PartOne + x2PartTwo
 	} else {
-		isI = false
-		x1 = (float64(-b) + math.Sqrt(math.Pow(float64(b), 2)-4*float64(a)*float64(c))) / (float64(2 * a))
-		x2 = (float64(-b) - math.Sqrt(math.Pow(float64(b), 2)-4*float64(a)*float64(c))) / (float64(2 * a))
+		x1 = FunctionsPackage.FloatToString((float64(-b) + math.Sqrt(math.Pow(float64(b), 2)-4*float64(a)*float64(c))) / (float64(2 * a)))
+		x2 = FunctionsPackage.FloatToString((float64(-b) - math.Sqrt(math.Pow(float64(b), 2)-4*float64(a)*float64(c))) / (float64(2 * a)))
 	}
 	return
+
 }
